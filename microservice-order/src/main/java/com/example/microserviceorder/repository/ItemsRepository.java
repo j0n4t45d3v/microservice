3177cea36974a9f7b01004846d4577471bbeb2d2
Jonatas <delimajonatas875@gmail.com>
2023-05-04 20:49:52 -0300

    repositorio itens

---
+++ microservice-order/src/main/java/com/example/microserviceorder/repository/ItemsRepository.java
@@ -0,0 +1,9 @@
+package com.example.microserviceorder.repository;
+
+import com.example.microserviceorder.model.Items;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemsRepository extends JpaRepository<Items, Long> {
+}
