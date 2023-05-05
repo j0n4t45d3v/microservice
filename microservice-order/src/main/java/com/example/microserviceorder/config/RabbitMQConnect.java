5790b03fb2fe58c04281e1a4dbe7eddbc8d39270
Jonatas <delimajonatas875@gmail.com>
2023-05-05 15:35:30 -0300

    add rabbitMQ

---
+++ microservice-order/src/main/java/com/example/microserviceorder/config/RabbitMQConnect.java
@@ -0,0 +1,8 @@
+package com.example.microserviceorder.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQConnect {
+
+}
---
+++ microservice-order/src/main/java/com/example/microserviceorder/service/RabbitMQService.java
@@ -0,0 +1,9 @@
+package com.example.microserviceorder.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQService {
+
+
+}
