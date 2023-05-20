8ea92e52c1e82fe836a9788ef90d675b370938a0
Jonatas <delimajonatas875@gmail.com>
2023-05-20 12:31:08 -0300

    fazendo a construção dos container no Dockerfile e add server de registry e config

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 ./mvnw clean package #comando para buildar todos os projetos
 cd ./docker
 docker-compose up --build -d #comando para subir todos os containers
+docker-compose ps #comando para listar todos os containers
