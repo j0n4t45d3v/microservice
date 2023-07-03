16626bd6ae0f5b5f13a009b266f0af32b4fe985b
Jonatas <delimajonatas875@gmail.com>
2023-07-03 20:19:00 -0300

    product-service: add timestamp no record Error.java

--- microservice/user-service/src/main/java/com/microservice/userservice/error/Error.java
+++ microservice/user-service/src/main/java/com/microservice/userservice/error/Error.java
@@ -2,9 +2,12 @@ package com.microservice.userservice.error;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import java.time.LocalDateTime;
+
 @JsonPropertyOrder({"error", "status"})
 public record Error(
         String error,
-        Integer status
+        Integer status,
+        LocalDateTime timestamp
 ) {
 }
